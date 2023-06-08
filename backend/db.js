983d848af1696e12704c49939935a0788cefed4e
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-06-08 21:27:20 +0500

    adding data validator using express-validator

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI = "mongodb://localhost:27017";
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
--- backend/routes/auths.js
+++ backend/routes/auths.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 // Create a user using POST "/api/auths". Doesn't require Auth
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 }),
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    }).then(user => res.json(user))
+        .catch(err => {
+            console.log(err)
+            res.json({ error: "Please enter a unique value for email", success: false })
+        });
 });
-
 module.exports = router;
