839597f5ea48ff8d96dff682b543e87bb00b86c2
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-06-08 20:07:22 +0500

    storing data into database using mongoose model

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 // Available Routes
 app.use('/api/auths', require('./routes/auths'));
-app.use('/api/notes', require('./routes/notes'));
+// app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
--- backend/routes/auths.js
+++ backend/routes/auths.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    const obj = {
-        a: "this",
-        b: "is",
-        c: "a",
-        d: "auth"
-    }
-    res.json(obj);
+// Create a user using POST "/api/auths". Doesn't require Auth
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 });
 
 module.exports = router;
