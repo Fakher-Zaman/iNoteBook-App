3b6bac3f2e58f91133560d3009c576de1d547cad
Fakher-Zaman <fakherzamanofficial@gmail.com>
2023-06-13 21:09:52 +0500

    iNotebook: Adding NoteItem in a Separate Note component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react'
-import noteContext from "../context/notes/NoteContext";
+import React from 'react'
+import Notes from './Notes'
 
 function Home() {
-    const context = useContext(noteContext);
-    const { notes } = context;
     return (
         <div>
             <div className='container my-3'>
@@ -25,13 +23,7 @@ function Home() {
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
-
-            <div className='container my-3'>
-                <h2>Your Notes</h2>
-                {notes.map((note) => {
-                    return note.title
-                })}
-            </div>
+            <Notes />
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -6,8 +6,8 @@ const NoteState = (props) => {
         {
             "tag": "Personal",
             "_id": "64856ef50546dc2e0843ee91",
-            "title": "Workout",
-            "description": "Please wake up early tomorrow!",
+            "title": "Workout_1",
+            "description": "Please wake up early on monday!",
             "user": "64841d8dc4546424b0e03ad0",
             "date": "2023-06-11T06:51:33.868Z",
             "__v": 0
@@ -15,8 +15,53 @@ const NoteState = (props) => {
         {
             "tag": "Personal",
             "_id": "64857c953c52da19f004821f",
-            "title": "Workout",
-            "description": "Please wake up early tomorrow!",
+            "title": "Workout_2",
+            "description": "Please wake up early on tuesday!",
+            "user": "64841d8dc4546424b0e03ad0",
+            "date": "2023-06-11T07:49:41.156Z",
+            "__v": 0
+        },
+        {
+            "tag": "Personal",
+            "_id": "64857c953c52da19f004821f",
+            "title": "Workout_3",
+            "description": "Please wake up early on wednesday!",
+            "user": "64841d8dc4546424b0e03ad0",
+            "date": "2023-06-11T07:49:41.156Z",
+            "__v": 0
+        },
+        {
+            "tag": "Personal",
+            "_id": "64857c953c52da19f004821f",
+            "title": "Workout_4",
+            "description": "Please wake up early on thursday!",
+            "user": "64841d8dc4546424b0e03ad0",
+            "date": "2023-06-11T07:49:41.156Z",
+            "__v": 0
+        },
+        {
+            "tag": "Personal",
+            "_id": "64857c953c52da19f004821f",
+            "title": "Workout_5",
+            "description": "Please wake up early on friday!",
+            "user": "64841d8dc4546424b0e03ad0",
+            "date": "2023-06-11T07:49:41.156Z",
+            "__v": 0
+        },
+        {
+            "tag": "Personal",
+            "_id": "64857c953c52da19f004821f",
+            "title": "Workout_6",
+            "description": "Please wake up early on saturday!",
+            "user": "64841d8dc4546424b0e03ad0",
+            "date": "2023-06-11T07:49:41.156Z",
+            "__v": 0
+        },
+        {
+            "tag": "Personal",
+            "_id": "64857c953c52da19f004821f",
+            "title": "Workout_7",
+            "description": "Please wake up early on sunday!",
             "user": "64841d8dc4546424b0e03ad0",
             "date": "2023-06-11T07:49:41.156Z",
             "__v": 0
